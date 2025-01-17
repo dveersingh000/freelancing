8a71365216223039b797da15c9172abc64347b91
Dharamveer Singh <dveersingh000@gmail.com>
2025-01-17 18:18:12 +0530

    modified profile page

--- models/Profile.js
+++ models/Profile.js
@@ -2,27 +2,25 @@ const mongoose = require("mongoose");
 
 const profileSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  fullName: { type: String, required: true },
-  nricOrFinNumber: { type: String, required: true },
-  dateOfBirth: { type: Date, required: true },
-  gender: { type: String, enum: ["Male", "Female"], required: true },
-  postalCode: { type: String, required: true },
-  images: {
-    selfie: { type: String, required: true },
-    front: { type: String, required: true },
-    back: { type: String, required: true },
-    ploc: { type: String }, // for LTVP
-    studentCard: { type: String }, // for Student
+  dob: { type: Date, required: false },
+  gender: { type: String, enum: ['Male', 'Female'], required: false },
+  postalCode: { type: String, required: false },
+  nricNumber: { type: String, required: false },
+  nricImages: {
+    selfie: { type: String },
+    front: { type: String },
+    back: { type: String },
   },
-  extraFields: {
-    student: {
-      studentId: { type: String },
-      schoolName: { type: String },
-    },
-    ltvp: {
-      plocExpiryDate: { type: Date },
-    },
+  finNumber: { type: String, required: false },
+  finImages: {
+    front: { type: String },
+    back: { type: String },
   },
+  plocImage: { type: String, required: false },
+  plocExpiryDate: { type: Date, required: false },
+  studentIdNumber: { type: String, required: false },
+  schoolName: { type: String, required: false },
+  studentCardImage: { type: String, required: false },
 });
 
 module.exports = mongoose.model("Profile", profileSchema);
